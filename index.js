df7fc5ca6980a0411b930e34d29f9cbed01fa763
Hayden Lugo <hayden@lugowebdevelopment.com>
2021-10-12 21:55:33 +0200

    day one

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generatorMarkDown = require("./utils/generateMarkdown")
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [{
+    type: "input",
+    message: "Title of project:\t",
+    name: "Title"
+},  {
+    type: "input",
+    message: "What is your GitHub username:\t",
+    name: "User"
+},  {
+    type: "input",
+    message: "Description of project:\n\tWhat was your motivation?\n\tWhy did you build this project?\n\tWhat problem does it solve?\n\tWhat did you learn?\n\tDescription:\t",
+    name: "Description"
+},  {
+    type: "input",
+    message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running:\t",
+    name: "Installation"
+},  {
+    type: "input",
+    message: "Provide instructions and examples for use. Include screenshots as needed.\n\tTo add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:\n\t```md\n\t![alt text](assets/images/screenshot.png)\n\t```\n\tUsage:\t",
+    name: "Usage"
+},  {
+    type: "input",
+    message: "List your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.\nIf you followed tutorials, include links to those here as well.",
+    name: "Credits"
+},  {
+    type: "input",
+    message: "The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).",
+    name: "Licences"
+}
+];
+
+
+function writeToFile(fileName, data) {
+    fs.writeFile(filename, data, function(err) {
+        console.log(filename);
+        console.log(data);
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("success")
+        }
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(data) {
+        writeToFile("README.md", generatorMarkDown(data));
+        console.log(data);
+    });
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,23 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+# ${data.Title}
+https://github.com/${data.User}/${data.Title}
+# Description
+${data.Description}
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+# Installation
+The following necessary dependencies must be installed to run the application ${data.Installation}
+# Usage
+In order to use this app, ${data.Usage}
+# License
+This project is licensed under the ${data.Licenses} license.
+# Contributing
+Contributos: ${data.Credits}
 `;
 }
 
